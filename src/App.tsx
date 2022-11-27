b278b0be27e26c0bd4b76f5bef43413980122609
Joshua Morton <mortson@hotmail.co.uk>
2022-11-27 21:38:58 +0000

    deploy source code

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ function App() {
   const videoElement = (document.querySelector(".videoElement") as HTMLVideoElement);
   const [cameraEnabled, setCameraEnabled] = useState(false);
   const [displayConstraints, setDisplayConstraints] = useState('');
-
+  const [photoCapabilities, setPhotoCapabilities] = useState('');
+  const [trackCapabilities, setTrackCapabilities] = useState('');
+  const [facingModeCapas, setFacingModeCapas] = useState('');
+  
   const videoConstraints = { 
-      // width: 720,
-      // height: 480,
-      video: true,
-      torch: true,
-      facingMode: { exact: "environment" }
-     };
-     
+    width: 720,
+    height: 480,
+    video: true,
+    torch: true,
+    facingMode: { exact: "environment" }
+  };
+  
   const getConstraints = () => {
     const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
     let str: string = '';
@@ -24,7 +27,7 @@ function App() {
     }
     setDisplayConstraints(str);
   }
-
+  
   const getActiveConstraints = () => {
     let str: string = '';
     for (const [key, value] of Object.entries(videoConstraints)) {
@@ -33,31 +36,66 @@ function App() {
     console.log(videoConstraints);
     setDisplayConstraints(str);
   }
+  
 
+  const displayTrackCapabilities = async (track: MediaStreamTrack) => {
+    let caps = track.getCapabilities();
+    console.log(caps);
+    let str = '';
 
- useEffect(() => {
-  /**Check for a user camera first */
+    for (let [key, value] of Object.entries(caps)){
+      str += `${key}: ${value}, `;
+    }
+  
+    setTrackCapabilities(str);
+  }
+
+  const facingModeCaps = async (track: MediaStreamTrack) => {
+    let caps = track.getCapabilities();
+    let facingMode = caps.facingMode;
+
+    let str = facingMode?.forEach(str => str);
+  }
+  
+  
+  const displayPhotoCapabilities = async (captureDevice: ImageCapture) => {
+    let caps = await captureDevice.getPhotoCapabilities();
+    console.log(caps);
+    let str = '';
+  
+    for (let [key, value] of Object.entries(caps)){
+      str += `${key}: ${value}, `;
+    }
+  
+    setPhotoCapabilities(str);
+  }
+
+  useEffect(() => {
+    /**Check for a user camera first */
     let hasCamera = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
     setCameraEnabled(hasCamera);
-
+    
+    
     navigator.mediaDevices.getUserMedia(videoConstraints)
       .then((stream) => {
-        let track = stream.getVideoTracks()[0];
-        track.applyConstraints(videoConstraints);
+          let track = stream.getVideoTracks()[0];
+          
+        let caps = track.getCapabilities();
+        console.log(caps);
 
-        videoElement.srcObject = stream;
-        track.applyConstraints(videoConstraints);
+          videoElement.srcObject = stream;
+          
+          let captureDevice = new ImageCapture(track);
 
-        let capabilities = track.getCapabilities();
-        console.log(capabilities);
-        let captureDevice = new ImageCapture(track);
-        
-        const photoCapabilities = () =>  captureDevice.getPhotoCapabilities();
-        
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+          const getEm = () => captureDevice.getPhotoCapabilities();
+          console.log(getEm());
+          displayTrackCapabilities(track);
+          facingModeCaps(track);
+          displayPhotoCapabilities(captureDevice);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
 })
   return (
     <div className="App">
@@ -69,9 +107,11 @@ function App() {
           Log Active Constraints
           </button>
       </div>
-      <video className="videoElement" autoPlay muted playsInline></video>
+      <video width={videoConstraints.width} height={videoConstraints.height} className="videoElement" autoPlay muted playsInline></video>
       <p>Constraints: {displayConstraints}</p>
-      <p></p>
+      <p>Track Caps: {trackCapabilities}</p>
+      <p>PhotoCaps: {photoCapabilities} </p>
+      <p>Facing Modes: {facingModeCapas}</p>
     </div>
   )
 }
