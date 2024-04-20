06b59836ed04bb0ac54ee6ea25c2215b2e8f5571
PIT-LPT-057\JoshMorton <josh.morton@propeltech.co.uk>
2024-04-20 23:34:36 +0100

    bump packages, try some funky stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, MutableRefObject } from "react";
 import "./App.css";
 
 function App() {
-  const videoElement = document.querySelector(
-    ".videoElement"
-  ) as HTMLVideoElement;
+  const videoElement = useRef(
+    null
+  ) as MutableRefObject<HTMLVideoElement | null>;
+
   const outputImg = document.getElementById("outputImg") as HTMLImageElement;
   const [cameraEnabled, setCameraEnabled] = useState(false);
   const [takePicture, setTakePicture] = useState(false);
 
   /* Largely irrelevant Dev stuff*/
-
+  const [text, setText] = useState("");
+  const [torchables, setTorchables] = useState("");
   const [displayConstraints, setDisplayConstraints] = useState("");
   const [photoCapabilities, setPhotoCapabilities] = useState("");
   const [trackCapabilities, setTrackCapabilities] = useState("");
@@ -36,39 +38,16 @@ function App() {
     video: {
       deviceId: deviceId,
       facingMode: {
-        exact: "environment",
+        ideal: "environment",
       },
+      torch: { ideal: true },
       width: { ideal: 720 },
       height: { ideal: 480 },
-      exact: ["user", "environment"],
+      //exact: ["user", "environment"],
     },
   };
 
   const toggleTorch = () => setTorchMode(!torchMode);
-
-  const FlashMode = (() => {
-    let flm: FillLightMode = "off";
-
-    const _turnOff = () => {
-      flm = "off";
-    };
-    const _turnOn = () => {
-      flm = "flash";
-    };
-
-    function toggle() {
-      if (flm === "flash") {
-        _turnOff();
-        console.log(flm);
-      } else if (flm === "off") {
-        _turnOn();
-        console.log(flm);
-      }
-    }
-    //const toggle = () => { (fillLightMode === "flash") ? _turnOff() : _turnOn() };
-
-    return { flm, toggle };
-  })();
 
   const takePhoto = () => {
     setTakePicture(true);
@@ -86,121 +65,108 @@ function App() {
     setDisplayConstraints(str);
   };
 
-  const getActiveConstraints = () => {
-    let str: string = "";
-    for (const [key, value] of Object.entries(videoConstraints)) {
-      str += `${key}: ${value}, `;
-    }
-    console.log(videoConstraints);
-    setDisplayConstraints(str);
-  };
+  // const getActiveConstraints = () => {
+  //   let str: string = "";
+  //   for (const [key, value] of Object.entries(videoConstraints)) {
+  //     str += `${key}: ${value}, `;
+  //   }
+  //   console.log(videoConstraints);
+  //   setDisplayConstraints(str);
+  // };
 
-  const displayTrackCapabilities = async (track: MediaStreamTrack) => {
-    let caps = track.getCapabilities();
-    console.log(caps);
-    let str = "";
+  // const displayTrackCapabilities = async (track: MediaStreamTrack) => {
+  //   let caps = track.getCapabilities();
+  //   console.log(caps);
+  //   let str = "";
 
-    for (let [key, value] of Object.entries(caps)) {
-      str += `${key}: ${value}, `;
-    }
+  //   for (let [key, value] of Object.entries(caps)) {
+  //     str += `${key}: ${value}, `;
+  //   }
 
-    setTrackCapabilities(str);
-  };
+  //   setTrackCapabilities(str);
+  // };
 
-  const facingModeCaps = async (track: MediaStreamTrack) => {
-    let caps = track.getCapabilities();
-    let facingMode = caps.facingMode;
+  // const facingModeCaps = async (track: MediaStreamTrack) => {
+  //   let caps = track.getCapabilities();
+  //   let facingMode = caps.facingMode;
 
-    let str = facingMode?.join(", ");
-  };
+  //   let str = facingMode?.join(", ");
+  // };
 
-  const displayPhotoCapabilities = async (captureDevice: ImageCapture) => {
-    let caps = await captureDevice.getPhotoCapabilities();
-    console.log(caps);
-    let str = "";
+  // const displayPhotoCapabilities = async (captureDevice: ImageCapture) => {
+  //   let caps = await captureDevice.getPhotoCapabilities();
+  //   console.log(caps);
+  //   let str = "";
 
-    for (let [key, value] of Object.entries(caps)) {
-      str += `${key}: ${value}, `;
-    }
-    setPhotoCapabilities(str);
-  };
+  //   for (let [key, value] of Object.entries(caps)) {
+  //     str += `${key}: ${value}, `;
+  //   }
+  //   setPhotoCapabilities(str);
+  // };
 
-  const devicesEnum = () => {
-    let camera: any;
-    navigator.mediaDevices.enumerateDevices().then((devices) => {
-      const cameras = devices.filter((device) => device.kind === "videoinput");
-      //console.log(cameras[0])
-      camera = cameras[cameras.length - 1];
-    });
-    return camera ?? null;
-  };
+  // const devicesEnum = () => {
+  //   let camera: any;
+  //   navigator.mediaDevices.enumerateDevices().then((devices) => {
+  //     const cameras = devices.filter((device) => device.kind === "videoinput");
+  //     //console.log(cameras[0])
+  //     camera = cameras[cameras.length - 1];
+  //   });
+  //   return camera ?? null;
+  // };
 
-  const camera: any = devicesEnum();
+  // const camera: any = devicesEnum();
 
-  console.log(camera);
+  // console.log(camera);
 
   //Meat of the Component
 
-  useEffect(() => {
-    /**Check for a user camera first */
-    let hasCamera = !!(
-      navigator.mediaDevices && navigator.mediaDevices.getUserMedia
-    );
-    setCameraEnabled(hasCamera);
-
-    // if (!cameraEnabled) {
+  const torchWork = async () => {
+    // if (!!!navigator.mediaDevices) {
     //   return;
     // }
 
-    //ChooseFlash.FlashState.fillLightMode[0]
-    //Get user rear facing camera & stream it to videoElement
-    navigator.mediaDevices
-      .getUserMedia(videoConstraints)
-      .then((stream) => {
-        videoElement.srcObject = stream;
-        //Pass MediaStream Track into ImageCapture API
-        const tracks = stream.getVideoTracks();
-        let track = stream.getVideoTracks()[0];
-        let captureDevice = new ImageCapture(track);
+    const stream = await navigator.mediaDevices.getUserMedia(videoConstraints);
 
-        tracks.forEach((t) => {
-          t.applyConstraints({
-            advanced: [
-              {
-                torch: torchMode,
-              },
-            ],
-            torch: torchMode,
-          });
-        });
+    if (videoElement.current) {
+      videoElement.current.srcObject = stream;
+    }
 
-        //checks for State (set by take photo button) to utilise API to take photo
-        if (takePicture) {
-          captureDevice.takePhoto({ fillLightMode: "flash" }).then((blob) => {
-            outputImg.src = URL.createObjectURL(blob);
-          });
-          setTakePicture(false);
-        }
+    const tracks = stream.getVideoTracks();
 
-        //DEV Stuff Output track/media accessible properties
-        displayTrackCapabilities(track);
-        facingModeCaps(track);
-        displayPhotoCapabilities(captureDevice);
-      })
-      .catch((err) => {
-        console.error(err);
+    const devices = tracks.filter((x) => x.getCapabilities());
+    setText(devices.length.toString());
+
+    await devices[0].applyConstraints({
+      torch: true,
+      advanced: [
+        {
+          torch: true,
+        },
+      ],
+    });
+
+    const captureDevice = new ImageCapture(devices[0]);
+
+    if (takePicture) {
+      captureDevice.takePhoto({ fillLightMode: "flash" }).then((blob) => {
+        outputImg.src = URL.createObjectURL(blob);
       });
-  });
+      setTakePicture(false);
+    }
+  };
+
+  useEffect(() => {
+    torchWork();
+  }, [torchMode, takePicture]);
 
   return (
     <div className="App">
       <div>
+        Devices = {text}
         <button type="button" onClick={getConstraints}>
           Log Supported Constraints
         </button>
-        <button type="button" onClick={getActiveConstraints}>
-          Log Active Constraints
-        </button>
+        <button type="button">Log Active Constraints</button>
         <button type="button" onClick={takePhoto}>
           Take Photo
         </button>
@@ -209,6 +175,7 @@ function App() {
         </button>
       </div>
       <video
+        ref={videoElement}
         width={videoConstraints.video.width.ideal}
         height={videoConstraints.video.height.ideal}
         className="videoElement"
